c53bbd00d0ac325b7e98f8b8b566e9d961fa2b4f
marcelovieira <bolabuga@gmail.com>
2024-10-30 15:50:08 -0300

    criacao script para converter varias abas de dados de arquivo xlsx para um csv unico unido

---
+++ save_to_csv.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+caminho_arquivo_xlsx = '/mnt/c/Users/marcelovieira/Downloads/RECUP_ARREC_ENTRE_151_211.xlsx'
+caminho_arquivo_csv = '/mnt/c/Users/marcelovieira/Downloads/arquivo_unido.csv'
+
+try:
+    print("Lendo todas as abas do arquivo Excel...")
+    todas_as_abas = pd.read_excel(caminho_arquivo_xlsx, sheet_name=None)
+    print(f"Lidas {len(todas_as_abas)} abas.")
+
+    print("Concatenando dados...")
+    dados_unidos = pd.concat(todas_as_abas.values(), ignore_index=True)
+    print(f"Total de linhas após concatenação: {len(dados_unidos)}")
+
+    print("Salvando o arquivo CSV...")
+    dados_unidos.to_csv(caminho_arquivo_csv, index=False)
+    print("Arquivo CSV criado com sucesso:", caminho_arquivo_csv)
+except Exception as e:
+    print("Ocorreu um erro:", e)
+
+
+
